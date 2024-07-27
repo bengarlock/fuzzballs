9445deebe26f2166499075c391b2c817202a5de9
Ben Garlock <ben.garlock@gmail.com>
2024-07-27 17:49:12 -0400

    first commit

---
+++ app/HLSPlayer.js
@@ -0,0 +1,29 @@
+'use client'
+import React, { useEffect, useRef } from 'react';
+import Hls from 'hls.js';
+
+const HLSPlayer = ({ src, autoPlay, controls, width, height }) => {
+    const videoRef = useRef(null);
+
+    useEffect(() => {
+        if (Hls.isSupported()) {
+            const hls = new Hls();
+            hls.loadSource(src);
+            hls.attachMedia(videoRef.current);
+        } else if (videoRef.current.canPlayType('application/vnd.apple.mpegurl')) {
+            videoRef.current.src = src;
+        }
+    }, [src]);
+
+    return (
+        <video
+            ref={videoRef}
+            autoPlay={autoPlay}
+            controls={controls}
+            width={width}
+            height={height}
+        />
+    );
+};
+
+export default HLSPlayer;
