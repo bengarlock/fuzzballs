8154603bc0f6d86aa42b15959eb64f7dc60807f4
bgarlock <ben.garlock@gmail.com>
2024-07-28 19:54:35 -0400

    styling work

--- app/page.js
+++ app/page.js
@@ -5,14 +5,17 @@ import HLSPlayer from './HLSPlayer';
 
 const LiveStream = () => {
     return (
-        <div className='flex-wrap bg-gray-100 justify-center p-8'>
-            <HLSPlayer
-                src="https://bengarlock.com/live/index.m3u8"
-                autoPlay={true}
-                controls={false}
-                // width="100%"
-                height="auto"
-            />
+        <div className='flex flex-col bg-gray-100 justify-center p-8'>
+            <div className={'bg-amber-600'}>
+                <HLSPlayer
+                    src="https://bengarlock.com/live/index.m3u8"
+                    autoPlay={true}
+                    controls={false}
+                    width="100%"
+                    height="auto"
+                />
+            </div>
+
         </div>
 
     );
