b7d7d3d1b8b049b2ab5d063d431c2edc07672e85
bgarlock <ben.garlock@gmail.com>
2024-07-28 20:21:40 -0400

    styling work

--- app/page.js
+++ app/page.js
@@ -3,6 +3,14 @@
 import React from 'react';
 import HLSPlayer from './HLSPlayer';
 
+function daysSince(dateString) {
+    const startDate = new Date(dateString);
+    const currentDate = new Date();
+    const timeDifference = currentDate - startDate;
+    return Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+}
+
+
 const LiveStream = () => {
     return (
         <div className='flex flex-col bg-gray-800 p-8 min-h-screen'>
@@ -12,11 +20,17 @@ const LiveStream = () => {
                 md:text-2xl lg:text-4xl dark:text-white'>Welcome to the Chickie Cam!</h1>
                 <div>
                     <p>Hello! We are the Lavender Orpington Chicks!</p>
-                    <p className='p-2'>Our names are: </p>
-                    <p>Claire Ann - <span className={'text-red-600'}>Red</span></p>
-                    <p>Annabelle Bronstein - <span className={'text-purple-600'}>Purple</span></p>
-                    <p>Bunny MacDougal - <span className={'text-green-600'}>Green</span></p>
-                    <p>Magda - <span className={'text-blue-600'}>Blue</span></p>
+                    <p className='p-2'>The color of our leg bands helps to identify us.</p>
+                    <p>Our names are: </p>
+                    <p>
+                        Claire Ann - <span className={'text-red-600'}>Red, </span>
+                        Annabelle Bronstein - <span className={'text-purple-600'}>Purple, </span>
+                        Bunny MacDougal - <span className={'text-green-600'}>Green, </span>
+                        and Magda - <span className={'text-blue-600'}>Blue</span>
+                    </p>
+                    <p>
+                        We are {daysSince('2024-07-22')} days old
+                    </p>
                 </div>
             </div>
             <div className={'bg-grey-1000 '}>
