cad485e358282e32d1cc66fb3317f59de11252fe
bgarlock <ben.garlock@gmail.com>
2024-08-03 07:19:07 -0400

    styling fixes and update magda

--- app/page.js
+++ app/page.js
@@ -13,27 +13,25 @@ function daysSince(dateString) {
 
 const LiveStream = () => {
     return (
-        <div className='flex flex-col bg-gray-800 p-8 min-h-screen'>
-
-            <div className='text-center p-6'>
-                <h1 className='mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-400
-                md:text-2xl lg:text-4xl dark:text-white'>Welcome to the Chickie Cam!</h1>
+        <div className="flex flex-col bg-gray-800 p-8 min-h-screen w-full items-center">
+            <div className="text-center p-6">
+                <h1 className="mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-400 md:text-2xl lg:text-4xl dark:text-white">
+                    Welcome to the Chickie Cam!
+                </h1>
                 <div>
                     <p>Hello! We are the Lavender Orpington Chicks!</p>
-                    <p className='p-2'>The color of our leg bands helps to identify us.</p>
-                    <p>Our names are: </p>
+                    <p className="p-2">The color of our leg bands helps to identify us.</p>
+                    <p>Our names are:</p>
                     <p>
-                        Claire Ann - <span className={'text-red-600'}>Red, </span>
-                        Annabelle Bronstein - <span className={'text-purple-600'}>Purple, </span>
-                        Bunny MacDougal - <span className={'text-green-600'}>Green, </span>
-                        and Magda - <span className={'text-blue-600'}>Blue</span>
+                        Claire Ann - <span className="text-red-600">Red, </span>
+                        Annabelle Bronstein - <span className="text-purple-600">Purple, </span>
+                        Bunny MacDougal - <span className="text-green-600">Green, </span>
+                        and Magda - <span className="text-yellow-600">Yellow</span>
                     </p>
-                    <p>
-                        We are {daysSince('2024-07-22')} days old
-                    </p>
+                    <p>We are {daysSince('2024-07-22')} days old</p>
                 </div>
             </div>
-            <div className={'bg-grey-1000 '}>
+            <div className="flex flex-col bg-gray-1000 justify-center w-3/4">
                 <HLSPlayer
                     src="https://bengarlock.com/live/index.m3u8"
                     autoPlay={true}
@@ -43,6 +41,7 @@ const LiveStream = () => {
                 />
             </div>
         </div>
+
     );
 };
 
