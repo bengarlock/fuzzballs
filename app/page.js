95af9b6996dad491c243461fecf9a802d3e136c5
Ben Garlock <ben.garlock@gmail.com>
2025-05-15 18:18:15 -0400

    Refactor code formatting and add missing asset.
    Improved consistency in code formatting by removing unnecessary comments and adjusting spacing around imports and components. Added the missing image asset `miranda.jpeg` to the project.

--- app/page.js
+++ app/page.js
@@ -2,9 +2,9 @@
 import HLSPlayer from './HLSPlayer';
 import Weather from "@/app/weather";
 import Age from "@/app/age";
-import { globalStore } from "@/app/globalstore";
+import {globalStore} from "@/app/globalstore";
 import Brightness from "@/app/brightness";
-import { useEffect } from "react";
+import {useEffect} from "react";
 import Peckingorder from "@/app/peckingorder";
 import Image from 'next/image';
 import incognitoImage from '@/public/media/incognito.png';
@@ -12,7 +12,7 @@ import getIncognitoStatus from "@/app/admin/getIncognitoStatus";
 import Authorize from "@/app/admin/Authorize";
 
 const LiveStream = () => {
-    const { weather, incognitoJob, setIncognitoJob, authToken, setAuthToken } = globalStore();
+    const {weather, incognitoJob, setIncognitoJob, authToken, setAuthToken} = globalStore();
 
     useEffect(() => {
         const fetchAuthAndStatus = async () => {
@@ -42,13 +42,10 @@ const LiveStream = () => {
 
     return (
         <div className="relative flex flex-col items-center p-8 min-h-screen w-full">
-            {/* Background div with fixed position and opacity */}
             <div
                 className="fixed top-0 left-0 w-full h-full bg-cover bg-center opacity-50"
-                style={{ backgroundImage: "url('/fuzzballs/media/photos/headerbar.jpeg')" }}
+                style={{backgroundImage: "url('/fuzzballs/media/photos/headerbar.jpeg')"}}
             />
-
-            {/* Content container ensuring it stays on top */}
             <div className="relative z-10 flex flex-col justify-evenly items-center text-center w-full md:w-1/2">
                 <div className="flex flex-row">
                     <h1 className="mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-400 md:text-2xl lg:text-4xl dark:text-white">
@@ -60,7 +57,7 @@ const LiveStream = () => {
                     incognitoJob.running ? (
                         <>
                             <div className="mt-20 rounded-full overflow-hidden w-[300px] h-[300px]">
-                                <Image src={incognitoImage} alt="Incognito" />
+                                <Image src={incognitoImage} alt="Incognito"/>
                             </div>
                             <p>The Chickie Cam will be back soon!</p>
                         </>
@@ -68,37 +65,47 @@ const LiveStream = () => {
                         <>
                             <p>We are Lavender and Buff Orpington chickens</p>
                             <div className="m-2">
-                                <HLSPlayer src={renderURL()} autoPlay={true} controls={true} width="100%" height="auto" />
+                                <HLSPlayer
+                                    src={renderURL()}
+                                    autoPlay={true}
+                                    muted={true}
+                                    controls={true}
+                                    width="100%"
+                                    height="auto"
+                                />
                             </div>
-                            <Weather />
+                            <Weather/>
 
                             <div className="flex flex-col md:flex-row w-full justify-around">
-                                <div className="flex flex-col bg-purple-900 items-center rounded-xl p-2 flex-1 min-h-[150px] m-2">
+                                <div
+                                    className="flex flex-col bg-purple-900 items-center rounded-xl p-2 flex-1 min-h-[150px] m-2">
                                     <h1>Lavenders</h1>
                                     <div>Claire Anne - <span className="text-pink-500">Pink, </span></div>
                                     <div>Annabelle Bronstein - <span className="text-purple-600">Purple, </span></div>
                                     <div>Bunny MacDougal - <span className="text-green-600">Green, </span></div>
                                     <div>and Magda - <span className="text-yellow-300">Yellow</span></div>
-                                    <div><Age date={'2024-07-22'} /></div>
+                                    <div><Age date={'2024-07-22'}/></div>
                                 </div>
-                                <div className="flex flex-col bg-amber-600 items-center rounded-xl p-2 flex-1 min-h-[150px] m-2">
+                                <div
+                                    className="flex flex-col bg-amber-600 items-center rounded-xl p-2 flex-1 min-h-[150px] m-2">
                                     <h1>Buffs</h1>
                                     <div>Carrie - <span className="text-pink-500">Pink, </span></div>
                                     <div>Charlotte - <span className="text-purple-600">Purple, </span></div>
                                     <div>Samantha - <span className="text-green-600">Green, </span></div>
                                     <div>Miranda - <span className="text-red-800">Red</span></div>
-                                    <div><Age date={'2023-06-12'} /></div>
+                                    <div><Age date={'2023-06-12'}/></div>
                                 </div>
                             </div>
 
-                            <Peckingorder />
-                            <Brightness />
+                            <Peckingorder/>
+                            <Brightness/>
                         </>
                     )
                 ) : (
                     <div>
                         <div className="flex items-center justify-center mt-20">
-                            <Image className="rounded-full overflow-hidden w-[300px] h-[300px] animate-bounce" src={incognitoImage} alt="Loading" />
+                            <Image className="rounded-full overflow-hidden w-[300px] h-[300px] animate-bounce"
+                                   src={incognitoImage} alt="Loading"/>
                         </div>
                         <div>Loading...</div>
                     </div>
